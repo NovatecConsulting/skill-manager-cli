623d571825d9deddf19ddc9bbc26d937c025ec1f
Timo Freiberg <timo.freiberg@gmail.com>
2020-05-22 09:08:59 +0200

    Some more TUI work

--- skill-manager-tui/src/main.rs
+++ skill-manager-tui/src/main.rs
@@ -16,7 +16,7 @@ use skill_manager::{
 use skill_manager_in_memory::{employees::EmployeeDb, projects::ProjectDb, skills::SkillDb};
 use std::{
     fmt::{self, Display},
-    io, iter,
+    io,
 };
 use termion::{
     event::Key,
@@ -26,30 +26,31 @@ use termion::{
 };
 use tui::{
     backend::{Backend, TermionBackend},
-    layout::{Alignment, Constraint, Direction, Layout},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     widgets::{Block, Paragraph, Tabs, Text, Widget},
-    Terminal,
+    Frame, Terminal,
 };
 
 fn main() {
-    if let Err(e) = go() {
+    if let Err(e) = run() {
         eprintln!("{}", e);
     }
 }
 
 type Result<T> = anyhow::Result<T>;
 
-enum DataTab {
+#[derive(Copy, Clone)]
+enum Header {
     Skills,
     Projects,
     Employees,
 }
 
-impl DataTab {
+impl Header {
     const SIZE: u16 = 3;
 
-    fn render(selected_tab: &DataTab) -> Tabs<&'static str> {
+    fn new(selected_tab: &Header) -> Tabs<&'static str> {
         Tabs::default()
             .block(Block::default().title("Skill Manager"))
             .titles(&["[E]mployees", "[P]rojects", "[S]kills"])
@@ -58,25 +59,86 @@ impl DataTab {
     }
     fn ix(&self) -> usize {
         match self {
-            DataTab::Skills => 2,
-            DataTab::Projects => 1,
-            DataTab::Employees => 0,
+            Header::Skills => 2,
+            Header::Projects => 1,
+            Header::Employees => 0,
         }
     }
 }
 
-impl Display for DataTab {
+impl Display for Header {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{}",
             match self {
-                DataTab::Employees => "Employees",
-                DataTab::Projects => "Projects",
-                DataTab::Skills => "Skills",
+                Header::Employees => "Employees",
+                Header::Projects => "Projects",
+                Header::Skills => "Skills",
             }
         )
     }
+}
+
+struct List {
+    title: String,
+    data: Vec<String>,
+}
+
+impl List {
+    fn new(data: Vec<String>, state: &State) -> Self {
+        Self {
+            data,
+            title: state.open_tab.to_string(),
+        }
+    }
+    fn render<'a>(&self, mut f: &mut Frame<impl Backend>, chunk: Rect) {
+        let list: Vec<_> = self.data.iter().map(Text::raw).collect();
+        Paragraph::new(list.iter())
+            .block(Block::default().title(&self.title))
+            .alignment(Alignment::Left)
+            .wrap(false)
+            .render(&mut f, chunk);
+    }
+}
+
+struct InputField<'a> {
+    input: &'a str,
+    open_tab: Header,
+}
+
+impl<'a> InputField<'a> {
+    const SIZE: u16 = 1;
+    fn new(input: &'a str, open_tab: Header) -> Self {
+        Self { input, open_tab }
+    }
+
+    fn render(&mut self, mut f: &mut Frame<impl Backend>, chunk: Rect) -> (u16, u16) {
+        let paragraph_chunk = chunk;
+        let prefix = match &self.open_tab {
+            Header::Skills => "New Skill: ",
+            Header::Projects => "New Project: ",
+            Header::Employees => "New Employee: ",
+        };
+        Paragraph::new([Text::raw(prefix), Text::raw(self.input)].iter())
+            .style(Style::default())
+            .alignment(Alignment::Left)
+            .render(&mut f, chunk);
+
+        (
+            //TODO overflow alert!
+            paragraph_chunk.left()
+                + prefix.chars().count() as u16
+                + self.input.chars().count() as u16,
+            paragraph_chunk.top(),
+        )
+    }
+}
+
+struct HotkeyHelp;
+
+impl HotkeyHelp {
+    const SIZE: u16 = 1;
 }
 
 enum InputMode {
@@ -85,14 +147,14 @@ enum InputMode {
 }
 
 struct State {
-    open_tab: DataTab,
+    open_tab: Header,
     mode: InputMode,
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
-            open_tab: DataTab::Employees,
+            open_tab: Header::Employees,
             mode: InputMode::List,
         }
     }
@@ -104,6 +166,9 @@ impl State {
         match &mut self.mode {
             InputMode::Input(input) => match key {
                 Key::Esc => self.mode = InputMode::List,
+                Key::Backspace => {
+                    input.pop();
+                }
                 Key::Char('\n') => {
                     effects.push(Effect::SendInput(input.clone()));
                     self.mode = InputMode::List;
@@ -115,13 +180,13 @@ impl State {
             },
             InputMode::List => match key {
                 Key::Char('p') => {
-                    self.open_tab = DataTab::Projects;
+                    self.open_tab = Header::Projects;
                 }
                 Key::Char('s') => {
-                    self.open_tab = DataTab::Skills;
+                    self.open_tab = Header::Skills;
                 }
                 Key::Char('e') => {
-                    self.open_tab = DataTab::Employees;
+                    self.open_tab = Header::Employees;
                 }
                 Key::Char('+') => self.mode = InputMode::Input(String::new()),
                 Key::Esc => effects.push(Effect::Quit),
@@ -144,7 +209,7 @@ struct Db {
     employees: EmployeeDb,
 }
 
-fn go() -> Result<()> {
+fn run() -> Result<()> {
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = MouseTerminal::from(stdout);
     let stdout = AlternateScreen::from(stdout);
@@ -164,13 +229,13 @@ fn go() -> Result<()> {
             match effect {
                 Effect::Quit => return Ok(()),
                 Effect::SendInput(input) => match state.open_tab {
-                    DataTab::Skills => {
+                    Header::Skills => {
                         db.skills.add(SkillLabel(input))?;
                     }
-                    DataTab::Employees => {
+                    Header::Employees => {
                         create_employee(&mut db, input)?;
                     }
-                    DataTab::Projects => {
+                    Header::Projects => {
                         db.projects
                             .add(ProjectLabel(input), ProjectDescription("".into()))?;
                     }
@@ -204,15 +269,15 @@ fn draw(terminal: &mut Terminal<impl Backend>, state: &State, db: &Db) -> Result
 
         let layout = match &state.mode {
             InputMode::List => vec![
-                Constraint::Length(DataTab::SIZE),
-                Constraint::Percentage(90),
-                Constraint::Length(2),
+                Constraint::Length(Header::SIZE),
+                Constraint::Min(20),
+                Constraint::Length(HotkeyHelp::SIZE),
             ],
             InputMode::Input(_) => vec![
-                Constraint::Length(DataTab::SIZE),
-                Constraint::Percentage(85),
-                Constraint::Length(5),
-                Constraint::Length(2),
+                Constraint::Length(Header::SIZE),
+                Constraint::Min(20),
+                Constraint::Length(InputField::SIZE),
+                Constraint::Length(HotkeyHelp::SIZE),
             ],
         };
         let chunks = Layout::default()
@@ -221,56 +286,33 @@ fn draw(terminal: &mut Terminal<impl Backend>, state: &State, db: &Db) -> Result
             .constraints(layout)
             .split(size);
 
-        let mut tabs = DataTab::render(&state.open_tab);
+        let mut tabs = Header::new(&state.open_tab);
         tabs.render(&mut f, chunks[0]);
 
-        let list: Vec<_> = match state.open_tab {
-            DataTab::Skills => db
-                .skills
-                .find_skills()
-                .unwrap()
-                .into_iter()
-                .map(|s| format!("{}\n", s.label))
-                .collect(),
-            DataTab::Projects => db
-                .projects
-                .find_projects()
-                .unwrap()
-                .into_iter()
-                .map(|p| format!("{}\n", p.label))
-                .collect(),
-            DataTab::Employees => db
-                .employees
-                .find_employees()
-                .unwrap()
-                .into_iter()
-                .map(|e| format!("{} {}\n", e.first_name, e.last_name))
-                .collect(),
-        };
-        let list: Vec<_> = list.iter().map(Text::raw).collect();
-        Paragraph::new(list.iter())
-            .block(Block::default().title(&state.open_tab.to_string()))
-            .alignment(Alignment::Left)
-            .wrap(false)
-            .render(&mut f, chunks[1]);
+        let list = List::new(retrieve_data(&state, &db), &state);
+        list.render(&mut f, chunks[1]);
 
         if let InputMode::Input(i) = &state.mode {
-            let paragraph_chunk = chunks[2];
-            Paragraph::new(iter::once(&Text::raw(i)))
-                .block(Block::default().title("Input"))
-                .style(Style::default())
-                .alignment(Alignment::Left)
-                .wrap(true)
-                .render(&mut f, chunks[2]);
+            let mut input_field = InputField::new(i, state.open_tab);
+            let cursor_pos = input_field.render(&mut f, chunks[2]);
+            set_cursor = Some(cursor_pos);
 
-            set_cursor = Some((
-                //TODO overflow alert!
-                paragraph_chunk.left() + i.len() as u16 + 1,
-                paragraph_chunk.top() + 1,
-            ));
+            //let paragraph_chunk = chunks[2];
+            //Paragraph::new(iter::once(&Text::raw(i)))
+            //    .block(Block::default().title("Input"))
+            //    .style(Style::default())
+            //    .alignment(Alignment::Left)
+            //    .wrap(true)
+            //    .render(&mut f, chunks[2]);
+
+            //set_cursor = Some((
+            //    //TODO overflow alert!
+            //    paragraph_chunk.left() + i.len() as u16 + 1,
+            //    paragraph_chunk.top() + 1,
+            //));
         };
 
-        let hotkey_hints = [Text::raw("[+] New entry")];
+        let hotkey_hints = [Text::raw("[+]New entry")];
         let mut hotkey_helper = Paragraph::new(hotkey_hints.iter());
         let hotkey_chunk_ix = match &state.mode {
             InputMode::List => 2,
@@ -287,4 +329,30 @@ fn draw(terminal: &mut Terminal<impl Backend>, state: &State, db: &Db) -> Result
     }
 
     Ok(())
+}
+
+fn retrieve_data(state: &State, db: &Db) -> Vec<String> {
+    match state.open_tab {
+        Header::Skills => db
+            .skills
+            .find_skills()
+            .unwrap()
+            .into_iter()
+            .map(|s| format!("{}\n", s.label))
+            .collect(),
+        Header::Projects => db
+            .projects
+            .find_projects()
+            .unwrap()
+            .into_iter()
+            .map(|p| format!("{}\n", p.label))
+            .collect(),
+        Header::Employees => db
+            .employees
+            .find_employees()
+            .unwrap()
+            .into_iter()
+            .map(|e| format!("{} {}\n", e.first_name, e.last_name))
+            .collect(),
+    }
 }
